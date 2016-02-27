308fea53c64eddeea397e986f3fc65393b98d1ae
Daniel Corn <d@cundd.net>
2016-02-27 12:51:52 +0100

    Changed the server.sh to use the script from noshi

--- server.sh
+++ server.sh
@@ -1,13 +1,2 @@
-#!/bin/sh
-
-: ${PHP_BINARY="php"}
-HOST=$1
-PORT=$2
-
-if [[ "$HOST" == "" ]]; then
-    HOST=localhost
-fi
-if [[ "$PORT" == "" ]]; then
-    PORT=9000
-fi
-$PHP_BINARY -S $HOST:$PORT -t .
+#!/usr/bin/env bash
+vendor/bin/noshi-server;
